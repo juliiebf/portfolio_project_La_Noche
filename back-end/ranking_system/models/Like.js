203fb6efda845744b4b0c309249f191fa0790343
Emma <furlan.emmanuelle@neuf.fr>
2025-10-14 15:28:34 +0200

    update

--- back-end/ranking_system/models/Like.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const likeSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  video: { type: mongoose.Schema.Types.ObjectId, ref: 'Video' },
-  createdAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Like', likeSchema);
--- back-end/ranking_system/routes/likeRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { likeVideo } = require('../controllers/likeController');
-
-router.post('/like_video', likeVideo);
-
-module.exports = router;
